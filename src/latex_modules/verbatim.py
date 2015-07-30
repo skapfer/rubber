071ad8a740bff117d5254bc9af3843d62ed2f869
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2015-07-30 17:09:29 +0200

    Style. verbatim.py: an explicit function is more readable than a lambda.

--- src/latex_modules/verbatim.py
+++ src/latex_modules/verbatim.py
@@ -8,8 +8,10 @@ def setup (document, context):
 	global doc
 	doc = document
 	doc.hook_macro('verbatiminput', 'a', hook_input)
-	doc.hook_begin('comment',
-		lambda loc: doc.h_begin_verbatim(loc, env='comment'))
+	doc.hook_begin('comment', hook_begin_comment)
+
+def hook_begin_comment (loc):
+        doc.h_begin_verbatim(loc, env='comment')
 
 def hook_input (loc, file):
 	if file.find('\\') < 0 and file.find('#') < 0:
