61458b26725ffd0074d10e49df64e5ba1af71c83
Sebastian Kapfer <sebastian.kapfer@fau.de>
2015-09-26 02:05:02 +0200

    fix pythontex after killing doc.target

--- src/latex_modules/pythontex.py
+++ src/latex_modules/pythontex.py
@@ -19,23 +19,24 @@ class Module (rubber.module_interface.Module):
 		self.doc = document
 
 	def pre_compile (self):
-		if not os.path.exists(self.doc.target + '.pytxcode'):
+		pytxcode = self.doc.basename (with_suffix=".pytxcode")
+		if not os.path.exists(pytxcode):
 			msg.info(_("Need compilation!"), pkg="pythontex")
 			self.force_compilation()
 		msg.info(_("running pythontex..."), pkg="pythontex")
 		self.run_pythontex()
-		self.doc.watch_file(self.doc.target + ".pytxcode")
+		self.doc.watch_file(pytxcode)
 		return True
 
 	def clean (self):
 		self.doc.remove_suffixes([".pytxcode"])
-		pythontex_files = 'pythontex-files-' + os.path.basename(self.doc.target)
+		pythontex_files = 'pythontex-files-' + self.doc.basename ()
 		if os.path.exists(pythontex_files):
 			msg.log(_("removing tree %s") % pythontex_files)
 			shutil.rmtree(pythontex_files)
 
 	def run_pythontex(self):
-		call = ['pythontex', self.doc.target + '.tex', ]
+		call = ['pythontex', self.doc.source (), ]
 		msg.debug(_("pythontex call is '%s'") % ' '.join(call), pkg="pythontex")
 		if not self.doc.env.is_in_unsafe_mode_:
 			msg.error(_("the document tries to run external programs which could be dangerous.  use rubber --unsafe if the document is trusted."))
