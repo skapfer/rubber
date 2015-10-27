5f85274c9c86799b08e2260d7640993e8a250fe1
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2015-10-27 19:49:27 +0100

    Rely on distutils for all configuration, build and install, removing glue code between sh, make and python.

--- setup.py
+++ setup.py
@@ -5,107 +5,194 @@
 # configuration script a la autoconf and as a setup script a la Distutils.
 #
 # As the rest of Rubber, this script is covered by the GPL (see COPYING).
-# (c) Emmanuel Beffara, 2002--2006
+# Copyright 2002-2006 Emmanuel Beffara
+# Copyright 2008-2015 Sebastian Kapfer
+# Copyright 2015-2015 Nicolas Boulenguez
 
+import distutils.cmd
+import distutils.command.build
+import distutils.command.clean
+import distutils.command.install
+import distutils.core
+import distutils.dir_util
+import distutils.log
+import glob
+import os.path
 import re
-import string
-import sys
-import os.path, glob
 
-# The module `settings' is produced by the configuration script.
+# A file f is generated from f.in by replacing @author@, @version@ by
+# sensible values (as ./configure does in the autoconf world).
+files_with_substitutions = (
+    os.path.join ("doc", "man-en", "rubber.1"),
+    os.path.join ("doc", "man-en", "rubber-info.1"),
+    os.path.join ("doc", "man-fr", "rubber.1"),
+    os.path.join ("doc", "man-fr", "rubber-info.1"),
+    os.path.join ("doc", "rubber.texi"),
+    os.path.join ("src", "version.py"),
+)
 
-try:
-	import settings
-except ImportError:
-	pass
+manual_basename = os.path.join ("doc", "rubber.")
+doc_recipes = (
+    ("html", ("makeinfo", "--html", "--no-split")),
+    ("info", ("makeinfo", "--info")),
+    ("pdf",  ("texi2dvi", "--pdf", "--quiet", "--tidy")),
+    ("txt",  ("makeinfo", "--plaintext")),
+)
 
-###  system checking
+class build (distutils.command.build.build):
 
-def do_check ():
-	"""
-	Check that everything required for Rubber is present.
-	"""
-	print "The configure script is running, good."
-	try:
-		import distutils.core
-		print "The Distutils are installed, good."
-	except ImportError:
-		print """
-I could not import distutils.core. You need the Distutils to install Rubber.
-(You don't need them to run Rubber, so you can try configuring and installing
-it by hand if you are brave.)"""
-		return 1
-	return 0
+    user_options = distutils.command.build.build.user_options + [
+        ("html", None, "format HTML documentation"),
+        ("pdf",  None, "format PDF documentation"),
+        ("txt",  None, "format plain text documentation"),
+    ]
+    boolean_options = distutils.command.build.build.boolean_options \
+                      + ["html", "pdf", "txt"]
+    info = True
+    html = False
+    pdf  = False
+    txt  = False
 
+    def generate_files_with_substitutions (self, subs):
+        pattern = "|".join (subs.keys ())
+        pattern = "@(" + pattern + ")@"
+        pattern = re.compile (pattern)
+        def repl (match_object):
+            return subs [match_object.group (1)]
+        def func (in_path, out_path):
+            with open (in_path) as in_file:
+                with open (out_path, "w") as out_file:
+                    for in_line in in_file:
+                        out_line = pattern.sub (repl, in_line)
+                        out_file.write (out_line)
+        for out_path in files_with_substitutions:
+            in_path = out_path + ".in"
+            self.make_file (in_path, out_path, func, (in_path, out_path))
 
-###  substitution of variables in files
+    def generate_documentation (self):
+        infile = manual_basename + "texi"
+        for fmt, recipe in doc_recipes:
+            if getattr (self, fmt):
+                outfile = manual_basename + fmt
+                cmd = recipe + ("--output=" + outfile, infile)
+                self.make_file (infile, outfile, self.spawn, (cmd, ))
 
-def expand_vars (subst, str):
-	"""
-	Substitute all occurences of the pattern ${foo} in the string with the
-	value of the key associated key in the dictionary `subst' until there are
-	no such patterns any more.
-	"""
-	pattern = re.compile("\${(" + string.join(subst.keys(), "|") + ")}")
-	def repl (match, subst=subst):
-		return subst[match.group(1)]
-	str2 = pattern.sub(repl, str)
-	while str != str2:
-		str = str2
-		str2 = pattern.sub(repl, str)
-	return str
+    def run (self):
+        subs = {}
+        subs ["moddir"] = self.get_finalized_command ("install").moddir
+        for v in ("author", "author_email", "url", "version"):
+            subs [v] = getattr (self.distribution.metadata, "get_"+v) ()
+        self.generate_files_with_substitutions (subs)
 
-def make_files (subst, files):
-	"""
-	Apply the given substitution (passed as a dictionary) in the specified
-	files. The list enumerates the target files, for each target `foo' the
-	source is named `foo.in' using standard convention. The convention for
-	substitutions is the following:
-	- @bar@ --> sub['bar']
-	- @@quux@@ --> sub['quux']
-	  where all '${xyz}' are replaced by sub['xyz'] recursively
-	"""
-	import re
-	import string
+        distutils.command.build.build.run (self)
 
-	words = string.join(subst.keys(), "|")
-	pat1 = re.compile("@(" + words + ")@")
-	pat2 = re.compile("@@(" + words + ")@@")
+        self.generate_documentation ()
 
-	def repl (match, subst=subst):
-		return subst[match.group(1)]
-	def full_repl (match, subst=subst):
-		return expand_vars(subst, subst[match.group(1)])
+class install (distutils.command.install.install):
 
-	for file in files:
-		if not os.path.exists(file + ".in"):
-			continue
-		print "writing %s..." % file
-		with open (file + ".in") as input:
-			with open (file, "w") as output:
-				for line in input:
-					output.write(pat1.sub(repl, pat2.sub(full_repl, line)))
+    user_options = distutils.command.install.install.user_options + [
+        ("moddir=", None, "installation directory for .rub modules and rules.ini [$base/share/rubber]"),
+        ("mandir=", None, "installation directory for manual pages [$base/man]"),
+        ("infodir=", None, "installation directory for info manuals [$base/info]"),
+        ("docdir=", None, "installation directory for other documentation [$base/share/doc/rubber]"),
+    ]
+    moddir  = "$base/share/rubber"
+    mandir  = "$base/man"
+    infodir = "$base/info"
+    docdir  = "$base/share/doc/rubber"
 
-###  distutils setup
+    def finalize_options (self):
+        distutils.command.install.install.finalize_options (self)
+        self._expand_attrs (("moddir", "mandir", "infodir", "docdir"))
 
-def do_setup ():
-	"""
-	Run the setup() function from the distutils with appropriate arguments.
-	"""
-	from distutils.core import setup
-	try:
-		moddir = expand_vars(settings.sub, settings.sub["moddir"])
-		mandir = expand_vars(settings.sub, settings.sub["mandir"])
-		infodir = expand_vars(settings.sub, settings.sub["infodir"])
-	except NameError:
-		moddir = "rubber"
-		mandir = "man"
-		infodir = "info"
-	setup(
-		name = "rubber",
-		version = settings.sub["version"],
-		description = "an automated system for building LaTeX documents",
-		long_description = """\
+    def run (self):
+        build = self.get_finalized_command ("build")
+        assert self.distribution.data_files == None
+        self.distribution.data_files = [
+            (self.moddir + "/modules",
+             glob.glob (os.path.join ("data", "modules", "*.rub"))),
+            (self.moddir, ("data/rules.ini", )),
+            (self.mandir + "/man1", (
+                "doc/man-en/rubber.1",
+                "doc/man-en/rubber-info.1",
+                "doc/man-en/rubber-pipe.1",
+            )),
+            (self.mandir + "/fr/man1", (
+                "doc/man-fr/rubber.1",
+                "doc/man-fr/rubber-info.1",
+                "doc/man-fr/rubber-pipe.1",
+            )),
+            (self.infodir, (manual_basename + "info", )),
+        ]
+        otherdocs = [manual_basename + f for f in ("html", "pdf", "txt")
+                     if getattr (build, f)]
+        if len (otherdocs) > 0:
+            self.distribution.data_files.append ((self.docdir, otherdocs))
+        distutils.command.install.install.run (self)
+
+class clean (distutils.command.clean.clean):
+
+    def remove_tree (self, path):
+        if os.path.exists (path):
+            distutils.dir_util.remove_tree (path, dry_run=self.dry_run)
+        else:
+            distutils.log.debug ("'%s' does not exist -- can't clean it", path)
+
+    def remove_file (self, path):
+        if os.path.exists (path):
+            distutils.log.info ("removing '%s'", path)
+            if not self.dry_run:
+                os.remove (path)
+
+    def run (self):
+        distutils.command.clean.clean.run (self)
+
+        if self.all:
+            for f in files_with_substitutions:
+                self.remove_file (f)
+
+        for fmt, _ in doc_recipes:
+            self.remove_file (manual_basename + fmt)
+        self.remove_tree ("rubber.t2d")
+
+        for dirpath, dirnames, filenames in os.walk (os.curdir):
+            for filename in filenames:
+                ew = filename.endswith
+                if ew ("~") or ew (".pyc") or ew (".pyo"):
+                    self.remove_file (os.path.join (dirpath, filename))
+
+        self.remove_tree (os.path.join ("tests", "tmp"))
+
+class tar (distutils.cmd.Command):
+    description = "wrapper for git archive"
+    user_options = [
+        ("revision=",  None, "git tree-ish [HEAD]"),
+        ("extension=", None, "archive extension [tar.gz]"),
+    ]
+    revision  = "HEAD"
+    extension = "tar.gz"
+    def initialize_options (self):
+        pass
+    def finalize_options (self):
+        pass
+    def run (self):
+        version = self.distribution.metadata.get_version ()
+        self.spawn (("git", "archive", self.revision, "-9",
+                     "--prefix=rubber-" + version + "/",
+                     "--output=rubber-" + version + "." + self.extension))
+
+def extract_version_from_first_line (news, pattern):
+    with open (news, "r") as f:
+        line = f.readline ()
+    match = re.match (pattern, line)
+    assert match, "Line 1 of " + news + " does not match '" + pattern + "'."
+    return match.group (1)
+
+distutils.core.setup (
+    name = "rubber",
+    version = extract_version_from_first_line ("NEWS", r'^Version ([0-9.]+) '),
+    description = "an automated system for building LaTeX documents",
+    long_description = """\
 This is a building system for LaTeX documents. It is based on a routine that
 runs just as many compilations as necessary. The module system provides a
 great flexibility that virtually allows support for any package with no user
@@ -115,79 +202,27 @@ makeindex. A good number of standard packages are supported, including
 graphics/graphicx (with automatic conversion between various formats and
 Metapost compilation).\
 """,
-		author = settings.sub["maintainer"],
-		author_email = settings.sub["maintainer_email"],
-		url = settings.sub["url"],
-		license = "GPL",
-		packages = ["rubber", "rubber.converters", "rubber.latex_modules"],
-		package_dir = {"rubber": "src"},
-		scripts = ["rubber", "rubber-info", "rubber-pipe"],
-		data_files =
-		[(moddir + "/modules", glob.glob("data/modules/*.rub")),
-		 (moddir, ["data/rules.ini"]),
-		 (mandir + "/man1",
-			["doc/man-en/rubber.1", "doc/man-en/rubber-info.1", "doc/man-en/rubber-pipe.1"]),
-		 (mandir + "/fr/man1",
-			["doc/man-fr/rubber.1", "doc/man-fr/rubber-info.1", "doc/man-fr/rubber-pipe.1"]),
-		 (infodir, ["doc/rubber.info"])]
-		)
-
-
-###  command line
-
-# This is mainly the standard command line from the distutils, except that we
-# add specific commands for the configure script, namely:
-# - `check' for checking if the system is appropriate
-# - `config' to generate configuration-specific files
-# - `inst' to install the package according to the configuration's settings
-#          (here, the install prefix is the next argument)
-
-sub_files = [
-    "Makefile",
-    "doc/rubber.texi",
-    "doc/man-en/rubber.1",
-    "doc/man-en/rubber-info.1",
-    "doc/man-fr/rubber.1",
-    "doc/man-fr/rubber-info.1",
-    "doc/rubber.texi",
-    "src/version.py" ]
-
-if len(sys.argv) > 1:
-	cmd = sys.argv[1]
-	if cmd == "check":
-		ret = do_check()
-		sys.exit(ret)
-	elif cmd == "rm_sub_files":
-		for filename in sub_files:
-			try:
-				os.remove(filename)
-			except:
-				pass
-	elif cmd == "config":
-		sub = settings.sub
-		make_files(sub, sub_files)
-		print ("""
-Rubber is now configured. It will use the following Python interpreter:
-    %s
-It will be installed in the following directories:
-    the main script: %s
-    the modules:     %s
-    the man pages:   %s
-    the info pages:  %s
-(unless you specify otherwise when running `make install')""" %
-		(expand_vars(sub, sub["python"]),
-		 expand_vars(sub, sub["bindir"]),
-		 expand_vars(sub, sub["moddir"]),
-		 expand_vars(sub, sub["mandir"]),
-		 expand_vars(sub, sub["infodir"])))
-	elif cmd == "inst":
-		sub = settings.sub
-		sub["prefix"] = sys.argv[2]
-		del sys.argv[2]
-		sys.argv = sys.argv + ["--prefix", sub["prefix"]]
-		sys.argv[1] = "install"
-		do_setup()
-	else:
-		do_setup()
-else:
-	do_setup()
+    author = 'Sebastian Kapfer',
+    author_email = 'sebastian.kapfer@fau.de',
+    url = 'https://launchpad.net/rubber/',
+    license = "GPL",
+    packages = (
+        "rubber",
+        "rubber.converters",
+        "rubber.latex_modules",
+    ),
+    package_dir = {
+        "rubber" : "src",
+    },
+    scripts = (
+        "rubber",
+        "rubber-info",
+        "rubber-pipe",
+    ),
+    cmdclass = { c : globals () [c] for c in (
+        "build",
+        "install",
+        "clean",
+        "tar",
+    )},
+)
