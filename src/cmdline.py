e57c094a8349a8a343bf9a1e01ff6f19282d44cd
Sebastian Kapfer <sebastian.kapfer@fau.de>
2015-11-11 01:40:42 +0100

    merge command-line stuff into one file (finally)

--- src/cmdline.py
+++ src/cmdline.py
@@ -6,9 +6,11 @@
 This is the command line interface for Rubber.
 """
 
+import os
+import os.path
 import sys
 from sys import exit
-import os.path
+import re
 import string
 from getopt import getopt, GetoptError
 
@@ -456,3 +458,221 @@ class Build (Main):
 
 	def process_source (self, env):
 		self.build (env)
+
+re_rubtmp = re.compile("rubtmp(?P<num>[0-9]+)\\.")
+
+def make_name ():
+	"""
+	Return a base name suitable for a new compilation in the current
+	directory. The name will have the form "rubtmp" plus a number, such
+	that no file of this prefix exists.
+	"""
+	num = 0
+	for file in os.listdir("."):
+		m = re_rubtmp.match(file)
+		if m:
+			num = max(num, int(m.group("num")) + 1)
+	return "rubtmp%d" % num
+
+def dump_file (f_in, f_out):
+	"""
+	Dump the contents of a file object into another.
+	"""
+	for line in f_in.readlines():
+		f_out.write(line)
+
+class Pipe (Main):
+	def __init__ (self):
+		super (Pipe, self).__init__ (mode="pipe")
+		# FIXME why?
+		msg.level = 0
+
+	def help (self):
+		sys.stderr.write (_("""\
+This is Rubber version %s.
+usage: rubber-pipe [options]
+available options:
+  -b, --bzip2              compress the final document with bzip2
+  -c, --command=CMD        run the directive CMD before parsing (see man page)
+  -e, --epilogue=CMD       run the directive CMD after parsing
+  -z, --gzip               compress the final document
+  -h, --help               display this help
+      --into=DIR           go to directory DIR before compiling
+  -k, --keep               keep the temporary files after compiling
+  -l, --landscape          change paper orientation (if relevant)
+  -n, --maxerr=NUM         display at most NUM errors (default: 10)
+  -m, --module=MOD[:OPTS]  use module MOD (with options OPTS)
+      --only=SOURCES       only include the specified SOURCES
+  -o, --post=MOD[:OPTS]    postprocess with module MOD (with options OPTS)
+  -d, --pdf                produce a pdf (synonym for -m pdftex or -o ps2pdf)
+  -p, --ps                 process through dvips (synonym for -m dvips)
+  -q, --quiet              suppress messages
+  -r, --read=FILE          read additional directives from FILE
+  -S, --src-specials       enable insertion of source specials
+  -s, --short              display errors in a compact form
+  -I, --texpath=DIR        add DIR to the search path for LaTeX
+  -v, --verbose            increase verbosity
+      --version            print version information and exit
+""") % rubber.version.version)
+
+	def short_help (self):
+		# normally, Rubber prints a short help text if no arguments
+		# at all are given.  This is valid for rubber-pipe, though.
+		pass
+
+	def parse_opts (self, cmdline):
+		args = super (Pipe, self).parse_opts (cmdline)
+		self.keep_temp = False
+		# rubber-pipe doesn't take file arguments
+		for arg in args:
+			self.ignored_option (arg)
+		# --inplace nonsensical since we don't have a filename
+		if self.place is None:
+			self.illegal_option ("--inplace")
+		# hack: force is required by self.build
+		self.force = False
+		return [ "-" ]   # this will be stdin
+
+	def prepare_source (self, filename):
+		"""
+		Dump the standard input in a file, and set up that file
+		the same way we would normally process LaTeX sources.
+		"""
+		assert filename.endswith ("-")  # filename is ignored
+
+		try:
+			filename = make_name () + ".tex"
+			# note the tempfile name so we can remove it later
+			self.pipe_tempfile = filename
+			# copy stdin into the tempfile
+			srcfile = open (filename, "w")
+			msg.progress (_("saving the input in %s") % filename)
+			dump_file (sys.stdin, srcfile)
+			srcfile.close ()
+		except IOError:
+			msg.error (_("cannot create temporary file '%s'") % filename)
+			sys.exit (2)
+
+		return super (Pipe, self).prepare_source (filename)
+
+	def process_source (self, env):
+		"""
+		Build the document, and dump the result on stdout.
+		"""
+		try:
+			self.build (env)
+			filename = env.final.products[0]
+			try:
+				# dump the results on standard output
+				with open (filename, "r") as output:
+					dump_file (output, sys.stdout)
+			except IOError:
+				msg.error (_("error copying the product '%s' to stdout") % filename)
+				sys.exit (2)
+		finally:
+			# clean the intermediate files
+			if not self.keep_temp:
+				self.clean (env)
+				rubber.util.verbose_remove (self.pipe_tempfile)
+
+class Info (Main):
+	def __init__ (self):
+		super (Info, self).__init__ (mode="info")
+		# FIXME why?
+		self.max_errors = -1
+		# FIXME why?
+		msg.write = rubber.util.stdout_write
+
+	def short_help (self):
+		sys.stderr.write (_("""\
+usage: rubber-info [options] source
+For more information, try `rubber-info --help'.
+"""))
+		sys.exit (1)
+
+	def help (self):
+		sys.stderr.write (_("""\
+This is Rubber's information extractor version %s.
+usage: rubber-info [options] source
+available options:
+  all options accepted by rubber(1)
+actions:
+  --boxes     report overfull and underfull boxes
+  --check     report errors or warnings (default action)
+  --deps      show the target file's dependencies
+  --errors    show all errors that occured during compilation
+  --help      display this help
+  --refs      show the list of undefined references
+  --rules     print the dependency rules including intermediate results
+  --version   print the program's version and exit
+  --warnings  show all LaTeX warnings
+""") % rubber.version.version)
+
+	def parse_opts (self, cmdline):
+		self.info_action = None
+		ret = super (Info, self).parse_opts (cmdline)
+		if self.info_action is None:
+			self.info_action = "check"
+		return ret
+
+	# FIXME rewrite
+	def process_source (self, env):
+		if self.info_action == "deps":
+			from rubber.depend import Leaf
+			deps = [ k for k,n in env.depends.iteritems () if type (n) is Leaf ]
+			rubber.util.stdout_write (string.join (deps))
+
+		elif self.info_action == "rules":
+			seen = {}
+			next = [self.env.final]
+			while len(next) > 0:
+				node = next[0]
+				next = next[1:]
+				if seen.has_key(node):
+					continue
+				seen[node] = None
+				if len(node.sources) == 0:
+					continue
+				print ("\n%s:" % string.join(node.products))
+				print (string.join(node.sources))
+				next.extend(node.source_nodes())
+		else:
+			self.info_log (self.info_action)
+
+	# FIXME rewrite
+	def info_log (self, act):
+		"""
+		Check for a log file and extract information from it if it exists,
+		accroding to the argument's value.
+		"""
+		log = self.env.main.log
+		if not self.env.main.parse_log ():
+			msg.error(_("Parsing the log file failed"))
+			exit (2)
+
+		if act == "boxes":
+			if not msg.display_all(log.get_boxes()):
+				msg.info(_("There is no bad box."))
+		elif act == "check":
+			if msg.display_all(log.get_errors()): return 0
+			msg.info(_("There was no error."))
+			if msg.display_all(log.get_references()): return 0
+			msg.info(_("There is no undefined reference."))
+			if not msg.display_all(log.get_warnings()):
+				msg.info(_("There is no warning."))
+			if not msg.display_all(log.get_boxes()):
+				msg.info(_("There is no bad box."))
+		elif act == "errors":
+			if not msg.display_all(log.get_errors()):
+				msg.info(_("There was no error."))
+		elif act == "refs":
+			if not msg.display_all(log.get_references()):
+				msg.info(_("There is no undefined reference."))
+		elif act == "warnings":
+			if not msg.display_all(log.get_warnings()):
+				msg.info(_("There is no warning."))
+		else:
+			sys.stderr.write(_("\
+I don't know the action `%s'. This should not happen.\n") % act)
+			return 1
+		return 0
