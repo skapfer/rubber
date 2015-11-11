c4736c47ea980e709235ec6c648ae9aa7f32466d
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2015-11-11 10:39:43 +0100

    Share implementation for dvips and dvipdfm, so that both benefit of 694. Better error reporting if len (source) < 4.

---
+++ src/dvip_tool.py
@@ -0,0 +1,67 @@
+# This file is part of Rubber and thus covered by the GPL
+# (c) Emmanuel Beffara, 2002--2006
+# (c) Sebastian Kapfer 2015
+# vim:et:ts=4
+"""
+Abstract class providing a common implementation for
+rubber.latex_modules.dvips and rubber.latex_modules.dvipdfm.
+
+PostScript/PDF generation through dvips/odvips/dvipdfm with Rubber.
+
+When the name of the main compiler is "Omega" (instead of "TeX" for
+instance), then "odvips" is used instead of "dvips".
+"""
+
+import sys
+
+import abc
+from rubber import _, msg
+import rubber.depend
+import rubber.module_interface
+
+# FIXME: this class may probably be simplified a lot if inheriting
+# from rubber.depend.Shell instead of rubber.depend.Node.
+
+product_extension = { 'dvips':'ps', 'dvipdfm':'pdf' }
+paper_selection_option = { 'dvips':'-t', 'dvipdfm':'-p' }
+
+class Module (rubber.depend.Node, rubber.module_interface.Module):
+    # This class may not be instantiated directly, only subclassed.
+    __metaclass__ = abc.ABCMeta
+
+    def __init__ (self, document, context, tool):
+        super (Module, self).__init__ (document.env.depends)
+
+        self.tool = tool
+        if tool not in ('dvipdfm', 'dvips'):
+            msg.error (_('Unknown tool: %s') % tool)
+            sys.exit (2)
+        self.doc = document
+        self.source = self.doc.env.final.products[0]
+        if not self.source.endswith ('.dvi'):
+            msg.error (_('I can\'t use %s when not producing a DVI') % tool)
+            sys.exit (2)
+        self.doc.env.final = self
+        self.add_product (self.source [:-3] + product_extension [tool])
+        self.add_source (self.source)
+        self.extra_args = []
+
+    def do_options (self, *args):
+        self.extra_args.extend (args)
+
+    def run (self):
+        # build command line
+        tool = self.tool
+        if tool == 'dvips' and self.doc.vars ['engine'] == 'Omega':
+            tool = 'odvips'
+        cmd = [ tool ]
+        for opt in self.doc.vars['paper'].split ():
+            cmd.extend ((paper_selection_option [self.tool], opt))
+        cmd.extend (self.extra_args)
+        cmd.append (self.source)
+
+        # run
+        if self.doc.env.execute (cmd, kpse=1) != 0:
+            msg.error (_('%s failed on %s') % (tool, self.source))
+            return False
+        return True
--- src/latex_modules/dvipdfm.py
+++ src/latex_modules/dvipdfm.py
@@ -1,45 +1,5 @@
 # This file is part of Rubber and thus covered by the GPL
-# (c) Emmanuel Beffara, 2003--2006
-"""
-PDF generation through dvipdfm with Rubber.
-"""
-
-import sys
-
-from rubber import _, msg
-import rubber.depend
-import rubber.module_interface
-
-# FIXME: this class may probably be simplified a lot if inheriting
-# from rubber.depend.Shell instead of rubber.depend.Node.
-
-class Module (rubber.depend.Node, rubber.module_interface.Module):
-
-	def __init__ (self, document, context):
-		super (Module, self).__init__ (document.env.depends)
-
-		source = document.env.final.products[0]
-		document.env.final = self
-		self.add_product (source [:-3] + 'pdf')
-		self.add_source (source)
-		self.env = document.env
-		tool = 'dvipdfm'
-		self.cmd = [tool]
-		for opt in document.vars ['paper'].split ():
-			self.cmd.extend (('-p', opt))
-		self.cmd.append (source)
-		if source[-4:] != '.dvi':
-			msg.error(_("I can't use %s when not producing a DVI")%tool)
-			sys.exit(2)
-
-	def do_options (self, *args):
-		cmd = self.cmd [:-1]
-		cmd.extend (args)
-		cmd.append (self.cmd [-1])
-		self.cmd = cmd
-
-	def run (self):
-		if self.env.execute (self.cmd, kpse=1) != 0:
-			msg.error(_("dvipdfm failed on %s") % self.cmd [1])
-			return False
-		return True
+import rubber.dvip_tool
+class Module (rubber.dvip_tool.Module):
+    def __init__ (self, document, context):
+        super (Module, self).__init__ (document, context, self.__module__)
