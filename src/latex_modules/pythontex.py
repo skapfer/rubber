5ac1479aa043538b64789ccf606d690fac26396c
Sebastian Kapfer <sebastian.kapfer@fau.de>
2015-11-12 22:38:58 +0100

    update pythontex support for modern rubber

--- src/latex_modules/pythontex.py
+++ src/latex_modules/pythontex.py
@@ -1,5 +1,6 @@
 # This file is part of Rubber and thus covered by the GPL
 # (c) Ferdinand Schwenk, 2013
+# (c) Sebastian Kapfer 2015
 # vim: noet:ts=4
 """
 pythontex support for Rubber
@@ -9,39 +10,41 @@ from rubber import _, msg
 
 import os.path
 import shutil
-import subprocess
 
 import rubber.module_interface
 
-class Module (rubber.module_interface.Module):
-
-	def __init__ (self, document, context):
+class PythonTeXDep (rubber.depend.Shell):
+	def __init__ (self, set, document):
 		self.doc = document
+		self.tool = 'pythontex'
+		basename = self.doc.basename ()
+		super (PythonTeXDep, self).__init__ (set, [self.tool, basename ])
+		self.pytxcode = basename + '.pytxcode'
+		self.pythontex_files = 'pythontex-files-%s/' % basename
+		self.pytxmcr = self.pythontex_files + basename + '.pytxmcr'
 
-	def pre_compile (self):
-		pytxcode = self.doc.basename (with_suffix=".pytxcode")
-		if not os.path.exists(pytxcode):
-			msg.info(_("Need compilation!"), pkg="pythontex")
-			self.force_compilation()
-		msg.info(_("running pythontex..."), pkg="pythontex")
-		self.run_pythontex()
-		self.doc.watch_file(pytxcode)
-		return True
+		self.doc.add_product (self.pytxcode)
+		self.add_source (self.pytxcode, track_contents=True)
+		self.add_product (self.pytxmcr)
+		self.doc.add_source (self.pytxmcr)
+
+	def run (self):
+		# check if the input file exists. if not, refuse to run.
+		if not os.path.exists (self.sources[0]):
+			msg.info (_('input file for %s does not yet exist, deferring')
+				% self.tool, pkg='pythontex')
+			return True
+		if not self.doc.env.is_in_unsafe_mode_:
+			msg.error (_('The document tries to run embedded Python code which could be dangerous.  Use rubber --unsafe if the document is trusted.'))
+			return False
+		return super (PythonTeXDep, self).run ()
 
 	def clean (self):
-		self.doc.remove_suffixes([".pytxcode"])
-		pythontex_files = 'pythontex-files-' + self.doc.basename ()
-		if os.path.exists(pythontex_files):
-			msg.log(_("removing tree %s") % pythontex_files)
-			shutil.rmtree(pythontex_files)
+		msg.log (_('removing tree %s') % self.pythontex_files)
+		# FIXME proper error reporting
+		shutil.rmtree (self.pythontex_files, ignore_errors=True)
+		return super (PythonTeXDep, self).clean ()
 
-	def run_pythontex(self):
-		call = ['pythontex', self.doc.source (), ]
-		msg.debug(_("pythontex call is '%s'") % ' '.join(call), pkg="pythontex")
-		if not self.doc.env.is_in_unsafe_mode_:
-			msg.error(_("the document tries to run external programs which could be dangerous.  use rubber --unsafe if the document is trusted."))
-			return
-		subprocess.call(call)
-
-	def force_compilation(self):
-		self.doc.compile()
+class Module (rubber.module_interface.Module):
+	def __init__ (self, document, context):
+		PythonTeXDep (document.set, document)
