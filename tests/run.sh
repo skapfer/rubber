40f6a003e1530caf82f1f3d7dedcf133b899c29e
Nicolas Boulenguez <nicolas@debian.org>
2018-09-04 17:07:41 +0200

    Add a debchroot option to test script.
    This option installs all test dependencies available in Debian,
    and removes the tests requiring external dependencies.

--- tests/run.sh
+++ tests/run.sh
@@ -25,6 +25,19 @@ while [ 1 -le $# ]; do
             VERBOSE="$VERBOSE $1"
             shift
             ;;
+        --debchroot)
+            # Dependencies inside Debian.
+            apt install -y \
+                debhelper dh-python python3 texlive-latex-base asymptote \
+                biber imagemagick python-prompt-toolkit python-pygments \
+                python3-prompt-toolkit python3-pygments r-cran-knitr \
+                texlive-bibtex-extra texlive-binaries texlive-extra-utils \
+                texlive-latex-extra texlive-latex-recommended \
+                texlive-metapost texlive-omega texlive-pictures transfig
+            # Dependencies outside Debian.
+            rm -fr combine
+            shift
+            ;;
         *)
             break
     esac
