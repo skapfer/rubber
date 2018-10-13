a4686d5e09a3083a7fc9faa96f3b7532d5716ae5
Nicolas Boulenguez <nicolas@debian.org>
2018-10-13 09:36:08 +0200

    Test support for glossaries, fix cleaning and avoid obsolete onchange.

--- src/latex_modules/glossaries.py
+++ src/latex_modules/glossaries.py
@@ -1,9 +1,31 @@
+from rubber.util import _
+import logging
+msg = logging.getLogger (__name__)
+import os.path
+import rubber.depend
 import rubber.module_interface
-class Module (rubber.module_interface.Module):
-    def __init__ (self, document, opt):
 
-        glo = document.basename (with_suffix = '.glo')
-        document.watch_file (glo)
-        document.onchange_md5 [glo] = md5_file (glo)
+class Module (rubber.module_interface.Module):
+
+    def __init__ (self, document, opt):
+        job = document.basename ()
+        glo = job + '.glo'
+
+        document.add_product (job + '.ist')
+        document.add_source (glo)
+
+        dep = Dep (document.set, ('makeglossaries', job))
         # FIXME: does probably fail with --inplace and friends.
-        document.onchange_cmd [glo] = 'makeglossaries ' + document.basename ()
+        dep.add_product (glo)
+        dep.add_product (job + '.gls')
+        dep.add_product (job + '.glg')
+        dep.add_source (job + '.aux')
+
+class Dep (rubber.depend.Shell):
+
+    def run (self):
+        aux = self.command [1] + '.aux'
+        if not os.path.exists (aux):
+            msg.info (_("%s does not exist yet, deferring makeglossaries"), aux)
+            return True
+        return super ().run ()
