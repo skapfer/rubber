760924b7170a999926a053f5f1650d81e73fc757
Nicolas Boulenguez <nicolas@debian.org>
2019-01-02 14:36:48 +0100

    Simplify renaming of LaTeX modules.

--- src/latex_modules/graphicx.py
+++ src/latex_modules/graphicx.py
@@ -1,4 +1,2 @@
 # The 'graphics' and 'graphicx' packages are treated as equivalent.
-import rubber.latex_modules.graphics
-# No need to even inherit.
-Module = rubber.latex_modules.graphics.Module
+from rubber.latex_modules.graphics import Module
--- src/latex_modules/minitoc-hyper.py
+++ src/latex_modules/minitoc-hyper.py
@@ -1,4 +1,2 @@
 # The 'minitoc' and 'minitoc-hyper' packages are treated as equivalent.
-import rubber.latex_modules.minitoc
-# No need to even inherit.
-Module = rubber.latex_modules.minitoc.Module
+from rubber.latex_modules.minitoc import Module
